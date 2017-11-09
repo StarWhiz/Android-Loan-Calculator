4cd4a96da74aeaf75d87c2104da21bbb06996bb3
Tai <taidao90@gmail.com>
2017-11-08 17:16:40 -0800

    Editing XML So Far 4 Fields done

---
+++ app/src/main/java/cs137/dbzgroup/loancalculator/LoanCalculator.java
@@ -0,0 +1,13 @@
+package cs137.dbzgroup.loancalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoanCalculator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loan_calculator);
+    }
+}
