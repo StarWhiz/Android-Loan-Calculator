c742b9acf0aa75c8b867cd00d70a6499375aaac9
Tai <taidao90@gmail.com>
2017-11-08 19:07:20 -0800

    added reset and calculate buttons

--- app/src/main/java/cs137/dbzgroup/loancalculator/LoanCalculator.java
+++ app/src/main/java/cs137/dbzgroup/loancalculator/LoanCalculator.java
@@ -2,11 +2,40 @@ package cs137.dbzgroup.loancalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoanCalculator extends AppCompatActivity {
 
+    //input
+    double dataHomeValue, dataDownPayment, dataApr , dataTaxRate;
+    int dataTerms;
+    //output
+    double datatotalTax, datatotalInterest, datamonthlyPayment;
+    String dataDate;
+
+    //EditTextViews
+    EditText homeValue, downPayment, apr, taxRate, terms;
+    TextView totalTax, totalInterest, monthlyPayment, date;
+    Button calculateButton, resetButton;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        //Linking widgets to View...
+        homeValue = (EditText) findViewById(R.id.homeValue);
+        downPayment = (EditText) findViewById(R.id.downPayment);
+        apr = (EditText) findViewById(R.id.apr);
+        taxRate = (EditText) findViewById(R.id.taxRate);
+        terms = (EditText) findViewById(R.id.terms);
+
+        calculateButton = (Button) findViewById(R.id.calculateButton);
+        resetButton = (Button) findViewById(R.id.resetButton);
+        
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loan_calculator);
     }
